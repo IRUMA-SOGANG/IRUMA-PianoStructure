a811151eca44551acc639b644cb7ecc7553295bc
bestYun2 <owo1113@gmail.com>
2018-11-17 22:02:25 +0900

    Initial commit

---
+++ Key.java
@@ -0,0 +1,8 @@
+package com.sogang.capstone2.iruma;
+
+public class Piano_Individual {
+    public int sound; // 도=1, 레=2, 미=3, 파=4, 솔=5, 라=6, 시=7
+    public int octave; // 옥타브 피아노 왼->오 1~7
+    public int color; // 0=White, 1=Black
+    Boolean press; // 0=안 누른거 , 1=누른거
+}
---
+++ Piano.java
@@ -0,0 +1,9 @@
+package com.sogang.capstone2.iruma;
+
+import java.util.ArrayList;
+
+public class Piano{
+    ArrayList <Piano_Individual> keyList; //list type이 Piano_individual
+}
+
+
