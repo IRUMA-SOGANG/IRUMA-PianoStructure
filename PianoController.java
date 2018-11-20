32ed75a04b15a0d7d212bc77283960e1618fa026
최고윤이 <35629155+bestYun2@users.noreply.github.com>
2018-11-20 22:15:49 +0900

    Update PianoController.java

--- PianoController.java
+++ PianoController.java
@@ -1,33 +1,39 @@
+ // 소리 이외의 피아노 부분
+// controller 부분에서 소리 부분 다룰 수 있도록 만든다.
 package com.sogang.capstone2.iruma;
-import com.sogang.capstone2.iruma.Key;
 
-import java.util.Scanner;
 
-public class PianoController {
-    public void setUserOctave (Key startKey, Key endKey)
-    {
-        // 받은 첫음과 끝음에 새로운 음을 입력받아 새로운 범위를 설정한다.
-        
-        int startNote = 0 ;
-        int endNote = 0 ;
-
-        // 사용자로부터 입력을 받는다
-
-        Scanner scan = new Scanner(System.in);
-        System.out.println("새로운 범위를 선택해주세요.");
-
-        System.out.println("시작할 음을 입력해주세요.");
-        startNote = scan.nextInt();
-        System.out.println(startNote);
-
-        System.out.println("마지막 음을 입력해주세요.");
-        endNote = scan.nextInt();
-        System.out.println(endNote);
-
-        scan.close();
-
-        // 받은 입력을 데이터를 적용
-        startKey.setSound(startNote); // 설정 범위 첫 음 받아오기
-        endKey.setSound(endNote); // 설정 범위 끝 음 받아오기
+ public class PianoController {
+    public void setOctave (int octaveNum) {
+        // octaveNum = 옥타브 몇 개 생성할 것인지?
+        for ( int i = 0 ; i<octaveNum ; i++ ) {
+            new Key(1, i, false, false); //도
+            new Key(1, i, true, false); //도#
+            new Key(2, i, false, false); //레
+            new Key(2, i, true, false); //레#
+            new Key(3, i, false, false); //미
+            new Key(4, i, false, false); // 파
+            new Key(4, i, true, false); // 파#
+            new Key(5, i, false, false); //솔
+            new Key(5, i, true, false); //솔#
+            new Key(6, i, false, false); //라
+            new Key(6, i, true, false); //라#
+            new Key(7, i, false, false); //시
+        }
     }
+    // 사용자가 누르면 View에서 호출
+    public void isDown (Key keydown) {
+        // 눌렀을 때
+        keydown.down = true;
+        // 소리 내기
+        //AudioSoundPlayer 수정함 참고부탁!
+        AudioSoundPlayer.playNote(keydown.sound, keydown.octave);
+
+    }
+    // 사용자가 떼면 View에서 isUp호출
+    public void isUp (Key keyup){
+        // 손가락 뗐을 때
+        keyup.down = false;
+    }
+
 }
