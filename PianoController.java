95104025f813f3d99519acfeba0991539b3089de
최고윤이 <35629155+bestYun2@users.noreply.github.com>
2018-11-20 23:55:51 +0900

    Update PianoController.java

--- PianoController.java
+++ PianoController.java
@@ -4,9 +4,10 @@ package com.sogang.capstone2.iruma;
 
 
  public class PianoController {
+     // piano 옥타브 몇개를 생성할 것인지 그 수만 parameter로 받아 생성
     public void setOctave (int octaveNum) {
         // octaveNum = 옥타브 몇 개 생성할 것인지?
-        for ( int i = 0 ; i<octaveNum ; i++ ) {
+        for ( int i = 1 ; i <= octaveNum ; i++ ) {
             new Key(1, i, false, false); //도
             new Key(1, i, true, false); //도#
             new Key(2, i, false, false); //레
@@ -21,12 +22,11 @@ package com.sogang.capstone2.iruma;
             new Key(7, i, false, false); //시
         }
     }
-    // 사용자가 누르면 View에서 호출
+    // 사용자가 누르면 View에서 isDown호출
     public void isDown (Key keydown) {
         // 눌렀을 때
         keydown.down = true;
         // 소리 내기
-        //AudioSoundPlayer 수정함 참고부탁!
         AudioSoundPlayer.playNote(keydown.sound, keydown.octave);
 
     }
@@ -34,6 +34,8 @@ package com.sogang.capstone2.iruma;
     public void isUp (Key keyup){
         // 손가락 뗐을 때
         keyup.down = false;
+        // 소리 끄기
+        AudioSoundPlayer.stopNote(keyup.sound, keyup.octave);
     }
 
 }
