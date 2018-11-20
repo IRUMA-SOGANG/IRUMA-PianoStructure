b960268d61df3b71cef1a263c4bcc67ee240b3b6
최고윤이 <35629155+bestYun2@users.noreply.github.com>
2018-11-20 22:14:58 +0900

    Update Key.java

--- Key.java
+++ Key.java
@@ -1,21 +1,19 @@
 package com.sogang.capstone2.iruma;
-
 import android.graphics.RectF;
 
 public class Key {
-    public int sound; // 14개씩으로 끊어서 하도록 정함
-    // 도=1, 레=2, 미=3, 파=4, 솔=5, 라=6, 시=7 다음 옥타브엔 도 = 8 부터 시 = 14
-     // 옥타브 피아노 왼->오 1~7
-    public int color; // 0=White, 1=Black
-    public RectF rect;
-    Boolean down; // 0=안 누른거 , 1=누른거
+    public int sound; // 1~7 도 ~ 시
+    public int octave; // 옥타브
+    Boolean die = false; // true: white key, false: black key
+    Boolean down = false; // true: 누른거 , false: 안누른거
 
-    public Key(int sound, int color, RectF rect) {
+
+    public Key(int sound, int octave, Boolean die, Boolean down) {
         this.sound = sound;
-        this.color = color;
-        this.rect = rect;
+        this.octave = octave;
+        this.die = die;
+        this.down = down;
     }
-
 
     public int getSound() {
         return sound;
@@ -25,34 +23,27 @@ public class Key {
         this.sound = sound;
     }
 
-    /*public int getOctave() {
+    public int getOctave() {
         return octave;
     }
 
     public void setOctave(int octave) {
         this.octave = octave;
-    }*/
-
-    public int getColor() {
-        return color;
     }
 
-    public void setColor(int color) {
-        this.color = color;
+    public Boolean getDie() {
+        return die;
     }
 
-    public Boolean getPress() {
+    public void setDie(Boolean die) {
+        this.die = die;
+    }
+
+    public Boolean getDown() {
         return down;
     }
 
-    public void setPress(Boolean press) {
-        this.down = press;
-    }
-    public RectF getRect() {
-        return rect;
-    }
-
-    public void setRect(RectF rect) {
-        this.rect = rect;
+    public void setDown(Boolean down) {
+        this.down = down;
     }
 }
